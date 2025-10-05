fb709d5c0c183c3482e58c62ff025d9b870248c8
YW0905 <yoriwang0905@gmail.com>
2025-10-05 15:47:34 -0400

    restart

--- app/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import * as showFrame from "./utils/showFrame";
-import * as themes from "./utils/applyTheme";
-import * as parser from "./parser";
-import * as sidebarActions from "./sidebar-actions";
-
-import { showSuccess } from "./utils/showMessage";
--- app/scripts/utils/applyTheme.js
+++
@@ -1,5 +0,0 @@
-const theme = document
-    .querySelector("meta[name='page-theme']")
-    .getAttribute("content");
-
-document.body.classList.add(theme);
